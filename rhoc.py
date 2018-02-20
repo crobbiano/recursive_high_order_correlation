eae59515f42ee072f5666157c393a6dd5d141be5
Christopher Robbiano <iamchris@rams.colostate.edu>
2018-02-20 00:01:47 -0700

    Fixed some thing. Some things still broken

--- rhoc.py
+++ rhoc.py
@@ -10,18 +10,18 @@ def localCorrelation(mat1, mat2, max_distance=2):
     corrmat = np.zeros((rows, cols))
     for y in range(0, rows):
         for x in range(0, cols):
-            for i in range(y - max_distance, y + max_distance + 1):
-                for j in range(x - max_distance, x + max_distance + 1):
-                    corrmat[y, x] += mat1pad[y + max_distance, x + max_distance] * mat2pad[
-                        y - i + max_distance, x - j + max_distance]
+            for i in range(-max_distance, max_distance + 1):
+                for j in range(-max_distance, max_distance + 1):
+                    firstterm = mat1pad[y + max_distance, x + max_distance]
+                    secondterm = mat2pad[y + max_distance - i, x + max_distance - j]
+                    corrmat[y, x] += firstterm * secondterm
 
     return corrmat
 
 
-def calcPaths(frames, max_distance=2, threshold=2.5, recursion_order=5):
+def calcPaths(frames, max_distance=4, threshold=2.5, recursion_order=5):
     Y = []
-    Y.append(localCorrelation(frames[0], frames[1], max_distance))
-    for i in range(1, recursion_order - 1):
+    for i in range(0, recursion_order-1):
         Y.append(localCorrelation(frames[i], frames[i + 1], max_distance))
 
     return Y
@@ -32,11 +32,11 @@ if __name__ == "__main__":
     y = 5
     frames = []
     frames.append(np.array([[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]))
-    frames.append(np.array([[0, 1, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]))
-    frames.append(np.array([[0, 1, 1, 0], [0, 0, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0]]))
-    frames.append(np.array([[0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0]]))
-    frames.append(np.array([[0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1]]))
+    frames.append(np.array([[0, 0, 0, 0], [1, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]))
+    frames.append(np.array([[0, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0]]))
+    frames.append(np.array([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0]]))
+    frames.append(np.array([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1]]))
 
-    paths = calcPaths(frames, max_distance=1)
+    paths = calcPaths(frames, max_distance=2)
 
     print(frames)
